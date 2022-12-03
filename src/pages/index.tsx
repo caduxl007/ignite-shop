9a30bc54545e5af91cd3b809824fdff9f99a3f9b
caduxl007 <eduardo2020dev@gmail.com>
2022-12-03 00:16:26 -0300

    feat: add home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,28 @@
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
+
+import camiseta1 from "../assets/camisetas/1.png";
 
 export default function Home() {
   return (
-    <div >
-     <h1>Oi</h1>
-    </div>
-  )
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  );
 }
